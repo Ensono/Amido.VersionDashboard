8fcc2f319531cad966bf16dda590a6dc2cf37a90
Richard Slater <github@richard-slater.co.uk>
2015-08-28 16:29:26 +0100

    Added NInject 3.2.2

---
+++ src/Amido.VersionDashboard.Web/Domain/IDataStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Amido.VersionDashboard.Web.Models;
+
+namespace Amido.VersionDashboard.Web.Domain
+{
+    public interface IDataStore
+    {
+        IEnumerable<Uri> AllowedUris();
+        IEnumerable<DashboardData> Dashboards();
+        IEnumerable<Navbar> NavigationItems();
+    }
+}
