e02b7460b4dda0a0f32bddddb84efaeef3c9b9f6
Richard Slater <github@richard-slater.co.uk>
2015-10-17 16:48:41 +0100

    Reinstalled WebOptimization

---
+++ src/Amido.VersionDashboard.UnitTests/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Amido.VersionDashboard.UnitTests
+{
+    public class Test {
+        [Fact]
+        public void PassingTest() {
+            Assert.Equal(1, 1);
+        }
+    }
+}
